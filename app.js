950d3b817b3f14f2eef964a98afec6309fe897dd
BenaSPACE <BenaSPACE@gmail.com>
2023-07-24 16:50:49 -0400

    job done

---
+++ app.js
@@ -0,0 +1,22 @@
+var arr = [], arr2 = [], arr3 = [], arr4 = [];
+
+for (let i=1; i<=10; i++)
+{
+    arr.push(i);
+}
+
+let j=1;
+while (j<=10)
+{
+    arr2.push(j++);
+}
+
+for (let i = 11; i<=21; i+=2)
+{
+    arr3.push(i);
+}
+
+for (let i=15; i>=1; i-=2)
+{
+    arr4.push(i);
+}
